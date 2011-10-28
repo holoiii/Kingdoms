eacc7127ade50c0cd05d69a41c2e94d5ea3193a8
Albert <albertpeng.ap@gmail.com>
2011-10-28 14:44:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kingdoms::Application.config.secret_token = '78670ab02f8f25b212fae1268ff06ad6da155e666769b89eaa0c0c87dd01a13c04962270b7b770780ff94a71d31d480006b3e617f0b2282dd96c82ab0738643b'
