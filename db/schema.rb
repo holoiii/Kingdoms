a3461fa696989962bb2c953d8532dbea850d0448
Albert <albertpeng.ap@gmail.com>
2011-10-29 13:58:06 -0700

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111029000045) do
+ActiveRecord::Schema.define(:version => 20111029205545) do
+
+  create_table "kingdoms", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "resources", :force => true do |t|
+    t.string   "name"
+    t.integer  "amount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
